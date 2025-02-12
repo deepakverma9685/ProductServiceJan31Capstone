cade094b7b76e7924ef2b5d943e0ccfe2acfb33d
Abhishek Ghosh <abhishek.ghosh_1@scaler.com>
2025-02-12 23:10:31 +0530

    Created the API getProductById()

---
+++ src/main/java/com/scaler/productservicejan31capstone/ProductServiceJan31CapstoneApplication.java
@@ -0,0 +1,15 @@
+package com.scaler.productservicejan31capstone;
+
+import com.scaler.productservicejan31capstone.models.Product;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceJan31CapstoneApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ProductServiceJan31CapstoneApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/scaler/productservicejan31capstone/dtos/FakeStoreProductDto.java
@@ -0,0 +1,34 @@
+package com.scaler.productservicejan31capstone.dtos;
+
+import com.scaler.productservicejan31capstone.models.Category;
+import com.scaler.productservicejan31capstone.models.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FakeStoreProductDto
+{
+    private long id;
+    private String title;
+    private double price;
+    private String category;
+    private String description;
+    private String image;
+
+    public Product toProduct()
+    {
+        Product product = new Product();
+        product.setId(id);
+        product.setDescription(description);
+        product.setName(title);
+        product.setPrice(price);
+        product.setImageUrl(image);
+
+        Category category1 = new Category();
+        category1.setName(category);
+
+        product.setCategory(category1);
+        return product;
+    }
+}
